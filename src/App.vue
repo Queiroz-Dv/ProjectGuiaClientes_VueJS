a061ec08b1a95abe681dfde5cc609cad2ad714d4
Queiroz <kalifor_dbel@hotmail.com>
2022-03-10 21:10:27 -0300

    Implementando v-for

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,72 @@
 <template>
   <div id="app">
-    <h1>Guia Clientes</h1>
-    <Cliente/>
-    <Produto/>
+    <div v-for="(cliente, index) in clientes" :key="cliente.id">
+      <h4>{{ index + 1}}</h4>
+      <Cliente :cliente="cliente" />
+      <hr>
+      <h4> Edição: </h4>
+      <input type="text" v-model="cliente.nome">
+      <input type="text" v-model="cliente.email">
+    </div>
   </div>
 </template>
 
 <script>
-import Cliente from './components/Cliente.vue';
-import Produto from './components/Produto.vue';
+import Cliente from "./components/Cliente.vue";
+//import Produto from './components/Produto.vue';
 export default {
   name: "App",
+  data() {
+    return {
+      Objcliente: {
+        nome: "Eduardo Queiroz",
+        email: "queiroz.dev@outlook.com",
+        idade: 26,
+      },
+      Objcliente2: {
+        nome: "Naylane Andrade",
+        email: "nay@gmail.com",
+        idade: 22,
+      },
+      Objcliente3: {
+        nome: "Larissa Rocha",
+        email: "lhara@gmail.com",
+        idade: 24,
+      },
+      Objcliente4: {
+        nome: "Patrícia Ribeiro",
+        email: "patricia.ribeiro@gmail.com",
+        idade: 28,
+      },
+
+      clientes: [
+        {
+          id: 1,
+          nome: "Foster Smith",
+          email: "foster.dev@outlook.com",
+          idade: 36,
+        },
+        {
+          id: 2,
+          nome: "Helena Souza",
+          email: "helena@gmail.com",
+          idade: 27,
+        },
+        {
+          id: 3,
+          nome: " Daniela Silva",
+          email: "dani@gmail.com",
+          idade: 20,
+        },
+      ],
+    };
+  },
   components: {
     Cliente,
-    Produto
-  }
-}
+    //Produto
+  },
+};
 </script>
 
-<style>
+<style scoped>
 </style>
--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <h2>Clientes</h2>
-    {{}}
+  <div :class="{ cliente: !isPremium, 'cliente-premium': isPremium }">
+    <h4>Nome:{{ cliente.nome }}</h4>
+    <hr />
+    <p>Email: {{ cliente.email }}</p>
+    <p v-if="showIdade === true">Idade: {{ cliente.idade }}</p>
   </div>
 </template>
 
@@ -9,14 +11,32 @@
 export default {
   data() {
     return {
-      nome: "",
-      numero: "",
-      email: "",
-      idade: 0,
+      isPremium: false,
     };
+  },
+
+  props: {
+    cliente: Object,
+    showIdade: Boolean,
   },
 };
 </script>
 
 <style scoped>
+.cliente {
+  background-color: gainsboro;
+  max-width: 500px;
+  height: 200;
+  padding: 1%;
+  margin-top: 2%;
+}
+
+.cliente-premium {
+  background-color: darkkhaki;
+  color: whitesmoke;
+  max-width: 500px;
+  height: 200;
+  padding: 1%;
+  margin-top: 2%;
+}
 </style>
